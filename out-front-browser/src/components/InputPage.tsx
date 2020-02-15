8992361c60cd1ba3ee96192a45904151ed1a7423
Jordan Cason <jordan.cason@redjack.com>
2020-02-15 16:40:35 -0500

    add newWeb3

--- out-front-browser/src/components/InputPage.tsx
+++ out-front-browser/src/components/InputPage.tsx
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 import { Card, Form, Icon, Input, Button, Select } from 'antd';
 const Option = Select.Option
 import { FormComponentProps } from 'antd/lib/form';
-
+import crypto from 'crypto';
+import BigNumber from 'bignumber.js';
+import * as util from 'util'
+import web3 from 'web3';
 interface IInputPageProps extends FormComponentProps {
   match: any;
   web3: any;
   bnClient: any;
 }
 interface IInputState {
-  bnClient: any
+  bnClient?: any,
+  domain?: any,
+  siphonPermission?: any,
+  domainData?: any,
+  siphonPermissionData?: any,
+  types?: any
+  payload?: any,
+  result?: any
 }
 interface IFormValues {
   vaultAddress: string
@@ -32,11 +42,80 @@ function hasErrors(fieldsError: any) {
   return Object.keys(fieldsError).some((field: any) => fieldsError[field]);
 }
 
+
 export class InputPage extends Component<IInputPageProps, IInputState> {
+
   constructor(props: IInputPageProps) {
     super(props)
+    this.state = {}
     this.createSignedTransaction = this.createSignedTransaction.bind(this)
   }
+
+  createEIP712Data = async(values: any) => {
+    const newWeb3 = new web3((window as any).ethereum)
+    console.log('newWeb3: ', newWeb3)
+    const domainData = {
+      name: "Siphon",
+      version: "1.0.0",
+      chainId: await newWeb3.eth.net.getId(),
+      verifyingcontract: "0x2f79d27225543423c4caca23f9f58cea1cde6c56",
+    }
+
+    const accounts = await newWeb3.eth.getAccounts();
+    const siphonPermissionData = {
+      owner: accounts[0],
+      sender: '0xFb50029AAD6C7AfE2fBD319596D9eC024c2Da8Bd',
+      token: values.token,
+      to: values.vaultAddress,
+      expiration: Math.floor(Date.now() /1000) - 2592000,
+      nonce: new BigNumber(`0x${crypto.randomBytes(32).toString('hex')}`).toString(10),
+      fee: values.fee
+    }
+    await this.setState(() => ({
+      domainData,
+      siphonPermissionData,
+      types: {
+        EIP712Domain: [
+          { name: 'name', type: 'string' },
+          { name: 'version', type: 'string' },
+          { name: 'chainId', type: 'uint256' },
+          { name: 'verifyingContract', type: 'address' },
+        ],
+        SiphonPermission: [
+          { name: 'owner', type: 'address' },
+          { name: 'sender', type: 'address' },
+          { name: 'token', type: 'address' },
+          { name: 'to', type: 'address' },
+          { name: 'expiration', type: 'uint64' },
+          { name: 'nonce', type: 'uint256' },
+          { name: 'fee', type: 'uint256' },
+        ],
+      }
+    }))
+      console.log('state: ', this.state)
+  }
+  async signEIP712() {
+    const newWeb3 = new web3((window as any).ethereum)
+    const accounts = await newWeb3.eth.getAccounts();
+    const payload = {
+      types: this.state.types,
+      domain: this.state.domainData,
+      primaryType: 'SiphonPermission',
+      message: this.state.siphonPermissionData,
+    };
+    const data = JSON.stringify({
+      method: 'eth_signTypedData',
+      params: [accounts[0], payload],
+      from: accounts[0],
+    })
+    const { result } = await util.promisify((newWeb3 as any).currentProvider.send)(data);
+    await this.setState(() => ({
+      payload,
+      result
+    }))
+    console.log(this.state)
+  }
+
   componentDidMount() {
     // To disable submit button at the beginning.
     this.props.form.validateFields();
@@ -49,6 +128,8 @@ export class InputPage extends Component<IInputPageProps, IInputState> {
         console.log('Received values of form: ', values);
         console.log('this.props.bnClient: ', this.props.bnClient)
       }
+      this.createEIP712Data(values)
+      this.signEIP712()
     });
   }
   render() {
@@ -61,9 +142,10 @@ export class InputPage extends Component<IInputPageProps, IInputState> {
         <h2>InputPage</h2>
         <Card title="Create new watcher" bordered={false} style={{ width: '80%' }}>
           <p>create a new watcher</p>
-          <Form {...formItemLayout} onSubmit={this.createSignedTransaction}>
+          <Form onSubmit={this.createSignedTransaction}>
             <Form.Item validateStatus={usernameError ? 'error' : ''} help={usernameError || ''}>
               {getFieldDecorator('vaultAddress', {
+                initialValue: '0xFb50029AAD6C7AfE2fBD319596D9eC024c2Da8Bd',
                 rules: [{ required: true, message: 'Please enter your vault address!' }],
               })(
                 <Input
@@ -84,11 +166,11 @@ export class InputPage extends Component<IInputPageProps, IInputState> {
             </Form.Item>
             <Form.Item label="Token">
               {getFieldDecorator('token', {
+	        initialValue: 'ETH',
                 rules: [{ required: true, message: 'Please select a token!' }],
               })(
                 <Select
                   placeholder="Select a token"
-                  defaultValue="ETH"
                 >
                   <Option value="ETH">ETH</Option>
                   <Option value="TRX">TRX</Option>
